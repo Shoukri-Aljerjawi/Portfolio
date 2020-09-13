11fc4a48942e032bbcf3186acf269fb580c6b3d4
shoukri <saljarjawi@gmail.com>
2020-09-13 04:13:17 +0300

    banner

--- js/main.js
+++ js/main.js
@@ -3,4 +3,13 @@ $(document).ready(() => {
   $('.toggle-icon').click(() => {
     $('.nav').toggleClass('cllapse');
   });
+  //fixed navBar when scroll down
+  var hight = $('.banner').height();
+  $(window).scroll(() => {
+    if ($(this).scrollTop() > hight) {
+      $('.nav').addClass('fixed');
+    } else {
+      $('.nav').removeClass('fixed');
+    }
+  });
 });
