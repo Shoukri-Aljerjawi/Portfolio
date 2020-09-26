823967e27d96212804fcfd9d1719de8034f9a4a8
shoukri <saljarjawi@gmail.com>
2020-09-27 01:39:04 +0300

    fixedNavBar

--- js/main.js
+++ js/main.js
@@ -14,3 +14,16 @@ $(document).ready(() => {
     $('.nav-links').removeClass('mob-nav-active');
   });
 });
+
+var $navbar = $('.navbar');
+var $links = $('nav li a');
+$(window).scroll(function (event) {
+  var $current = $(this).scrollTop();
+  if ($current > 400) {
+    $navbar.addClass('scroll');
+    // $links.addClass('scroll');
+  } else {
+    $navbar.removeClass('scroll');
+    $links.removeClass('scroll');
+  }
+});
